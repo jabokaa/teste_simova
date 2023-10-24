03e4ff18b4f06fa2292b140eab45cbdfd28cccc9
João Beleño <joao.beleno@gmail.com>
2023-10-24 16:20:39 -0300

    Cadastro de apontamento front

--- public/assests/js/index.js
+++ public/assests/js/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     // Activate tooltip
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('#last_appointment').on('change', function(){
+        // verifica se esta checado
+        $('#end_date').hide();
+        if($(this).is(':checked')){
+            console.log('show');
+            $('#end_date').show();
+        }
+        else {
     
+            console.log('hide');
+        }
+    });
 });
