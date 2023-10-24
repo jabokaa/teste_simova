1f06c0dce572e0de1c65d22ee926b5cdb4f2906c
João Beleño <joao.beleno@gmail.com>
2023-10-24 13:48:48 -0300

    Criando models

---
+++ app/models/Appointment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+class Appointment extends Model{
+
+    protected $table = 'appointment';
+    protected $range = 2;
+
+}
---
+++ app/models/Employee.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models;
+
+class Employee extends Model{
+
+    protected $table = 'employee';
+
+}
