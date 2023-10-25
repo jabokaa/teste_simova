87ec62433057baa16b127ff62919a4f0ec9b27db
João Beleño <joao.beleno@gmail.com>
2023-10-24 21:51:24 -0300

    Refatorando

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -6,6 +6,10 @@ use PDO;
 
 class Connection{
 
+    /**
+     * Cria a conexão com o banco de dados
+     * @return PDO
+     */
     public static function connect(){
         $pdo = new PDO("mysql:host=localhost;dbname=simova_teste;charset=utf8", "root", "");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
